7898f5391ee2368e7160a0e39ec9a188e7194ebe
Max Jung <mail@maxjung.tech>
2024-04-22 17:20:21 +0200

    multiproject build

---
+++ services/product-comp-service/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.2.5"
+    id("io.spring.dependency-management") version "1.1.4"
+}
+
+group = "tech.maxjung.microservices.composite"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.projectreactor:reactor-test")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,6 @@
+rootProject.name = "microservices-spring-boot"
+
+include("services:product-service")
+include("services:recommendation-service")
+include("services:review-service")
+include("services:product-comp-service")
