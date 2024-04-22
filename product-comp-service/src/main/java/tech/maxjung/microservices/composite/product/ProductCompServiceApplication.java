dc8089c58f304f4651acc5c8628ef400b054ed7c
Max Jung <mail@maxjung.tech>
2024-04-22 14:42:28 +0200

    Initial commit

---
+++ product-comp-service/src/main/java/tech/maxjung/microservices/composite/product/ProductCompServiceApplication.java
@@ -0,0 +1,13 @@
+package tech.maxjung.microservices.composite.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductCompServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductCompServiceApplication.class, args);
+    }
+
+}
---
+++ review-service/src/test/java/tech/maxjung/microservices/core/review/ReviewServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.maxjung.microservices.core.review;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
