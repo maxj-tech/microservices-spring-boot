b006d4b6aa1b81ffb92e056840252067d1d3865c
Max Jung <mail@maxjung.tech>
2024-04-23 12:05:36 +0200

    impl of product service

--- services/product-service/src/main/java/tech/maxjung/microservices/core/product/ProductServiceApplication.java
+++ services/product-service/src/main/java/tech/maxjung/microservices/core/product/ProductServiceApplication.java
@@ -2,8 +2,10 @@ package tech.maxjung.microservices.core.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("tech.maxjung")
 public class ProductServiceApplication {
 
     public static void main(String[] args) {
