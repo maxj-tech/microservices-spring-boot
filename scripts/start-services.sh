84e0587633fc25c8e8b7bf6cb98b7ab0b4b5261f
Max Jung <mail@maxjung.tech>
2024-07-07 16:33:38 +0200

    adapt scripts to docker environment and update README.MD

--- scripts/start-services.sh
+++ scripts/start-services.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 # Ensure the script is executed from the project root directory
 cd "$(dirname "$0")/.."
@@ -6,23 +7,35 @@ cd "$(dirname "$0")/.."
 # Source service paths
 config_path="scripts/service-paths-config.sh"
 if [ -f "$config_path" ]; then
-    source "$config_path"
+  source "$config_path"
 else
-    echo "Configuration file not found at $config_path"
-    exit 1
+  echo "Configuration file not found at $config_path"
+  exit 1
+fi
+
+# Check for the "docker" flag
+docker=false
+if [[ $@ == *"docker"* ]]; then
+  docker=true
 fi
 
 # Perform clean or build if requested
-if [ "$1" == "clean" ]; then
-    ./gradlew clean build
-elif [ "$1" == "build" ]; then
-    ./gradlew build
+if [[ $@ == *"clean"* ]]; then
+  ./gradlew clean build
+elif [[ $@ == *"build"* ]]; then
+  ./gradlew build
 fi
 
-# Start the services and capture the PIDs
-java -jar $product_comp_service &
-java -jar $product_service &
-java -jar $recommendation_service &
-java -jar $review_service &
+# Start the services based on the flag
+if [ $docker == true ]; then
+  docker compose up -d
+else
+  java -jar $product_comp_service & comp_pid=$!
+  java -jar $product_service & prod_pid=$!
+  java -jar $recommendation_service & rec_pid=$!
+  java -jar $review_service & rev_pid=$!
+
+  wait $comp_pid $prod_pid $rec_pid $rev_pid
+fi
 
 echo "All services started successfully."
--- scripts/stop-services.sh
+++ scripts/stop-services.sh
@@ -1,28 +1,38 @@
 #!/bin/bash
+set -e
 
 # Ensure the script is executed from the project root directory
 cd "$(dirname "$0")/.."
 
-
 # Source service paths
 config_path="scripts/service-paths-config.sh"
 if [ -f "$config_path" ]; then
-    source "$config_path"
+  source "$config_path"
 else
-    echo "Configuration file not found at $config_path"
-    exit 1
+  echo "Configuration file not found at $config_path"
+  exit 1
 fi
 
 # Function to remove the wildcard from the path
 remove_wildcard() {
-    echo "${1%*.jar}" # This removes "*.jar" from the end of the string
+  echo "${1%*.jar}" # This removes "*.jar" from the end of the string
 }
 
 # Stop the services based on the JAR path without wildcards
 echo "Stopping services..."
-pkill -f "$(remove_wildcard "$product_comp_service")"
-pkill -f "$(remove_wildcard "$product_service")"
-pkill -f "$(remove_wildcard "$recommendation_service")"
-pkill -f "$(remove_wildcard "$review_service")"
+
+# Check the exit code of docker-compose down
+docker_compose_down_result=$(docker compose down --remove-orphans)
+docker_compose_down_exit_code=$?
+
+if [ $docker_compose_down_exit_code -eq 0 ]; then
+  # Use pkill only if docker-compose down might have left behind processes
+  pkill -f "$(remove_wildcard "$product_comp_service")"
+  pkill -f "$(remove_wildcard "$product_service")"
+  pkill -f "$(remove_wildcard "$recommendation_service")"
+  pkill -f "$(remove_wildcard "$review_service")"
+fi
+
+
 
 echo "All services stopped."
