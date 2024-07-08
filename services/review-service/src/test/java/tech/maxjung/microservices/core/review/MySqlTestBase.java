fd74240456516f95dd615179d887716893e5c3a4
Max Jung <mail@maxjung.tech>
2024-07-08 16:59:14 +0200

    db persistence for review-service

---
+++ services/review-service/src/test/java/tech/maxjung/microservices/core/review/MySqlTestBase.java
@@ -0,0 +1,24 @@
+package tech.maxjung.microservices.core.review;
+
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.JdbcDatabaseContainer;
+import org.testcontainers.containers.MySQLContainer;
+
+
+public abstract class MySqlTestBase {
+
+  private static JdbcDatabaseContainer database = new MySQLContainer("mysql:8.4.1").withStartupTimeoutSeconds(300);
+
+  static {
+    database.start();
+  }
+
+  @DynamicPropertySource
+  static void databaseProperties(DynamicPropertyRegistry registry) {
+    registry.add("spring.datasource.url", database::getJdbcUrl);
+    registry.add("spring.datasource.username", database::getUsername);
+    registry.add("spring.datasource.password", database::getPassword);
+  }
+
+}
