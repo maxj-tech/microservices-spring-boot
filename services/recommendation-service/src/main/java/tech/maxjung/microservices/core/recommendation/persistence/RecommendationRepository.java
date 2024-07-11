a6ec810004f858b73748fdb224888222ac260760
Max Jung <mail@maxjung.tech>
2024-07-11 10:48:34 +0200

    persistence with mongoDb for recommendation service

---
+++ services/recommendation-service/src/main/java/tech/maxjung/microservices/core/recommendation/persistence/RecommendationRepository.java
@@ -0,0 +1,13 @@
+package tech.maxjung.microservices.core.recommendation.persistence;
+
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+public interface RecommendationRepository extends CrudRepository<RecommendationEntity, String> {
+
+	@Transactional(readOnly = true)
+	List<RecommendationEntity> findByProductId(int productId);
+}
